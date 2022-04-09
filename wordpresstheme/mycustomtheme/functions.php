028d2ccbbd4c3a895c6f1f2f43800cf6866eff30
thinhshark <42465850+thinhshark@users.noreply.github.com>
2022-04-09 11:07:20 +0700

    theme

---
+++ wordpresstheme/mycustomtheme/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+//Theme support
+add_theme_support( 'custom-logo',array(
+        'height'               => 100,
+        'width'                => 400,
+        'flex-height'          => true,
+        'flex-width'           => true,
+        'header-text'          => array( 'site-title', 'site-description' ),
+    ));
+ 
